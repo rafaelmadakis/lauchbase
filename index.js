2d39576ba62eda1e93d22855b817123f14c97f20
rafaelmadakis <rafaelmadakis@gmail.com>
2020-05-10 19:49:00 -0300

    Iniciando com javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+// Criar um programa que calcula a média
+// das notas entre os alunos e envia
+// mensagem do calculo da média
+// se a media for maior que 5, parabenizar a turma
+
+const aluno1 = 'Mayk'
+const notaAluno01 = 9.8
+
+const aluno2 = 'Diego'
+const notaAluno02 = 10
+
+const aluno3 = 'Fulano'
+const notaAluno03 = 2
+
+
+
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) /  3
+
+if (media > 5){
+ console.log(`A nota foi de ${media}. Parabéns`)
+}else { 
+ console.log('A média é menor que 5')
+}
+
+
+
+
